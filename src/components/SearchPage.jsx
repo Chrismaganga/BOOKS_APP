4b3f0577eb76bc4eca53dcd1bdb7d76d067ce565
Chrismaganga <chrismaganga3@gmail.com>
2024-12-02 09:48:38 +0200

    add css and searchPage upadates

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -6,7 +6,6 @@ import { debounce } from "lodash";
 import * as BookAPI from "../utils/BookAPI";
 import { FaArrowLeft } from "react-icons/fa";
 
-
 const SearchPage = ({ booksOnShelves, onShelfChange }) => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
@@ -60,51 +59,47 @@ const SearchPage = ({ booksOnShelves, onShelfChange }) => {
   };
 
   const handleShelfChange = (book, shelf) => {
-    if (book && shelf !== "none") {
+    if (book) {
       onShelfChange(book, shelf);
-      setResults((prevResults) => prevResults.filter((b) => b.id.name !== book.id.name));
-    } else if (book && shelf === "none") {
-      onShelfChange(book, shelf);
-      setResults((prevResults) => [
-        ...prevResults,
-        { ...book, shelf: "none" },
-      ]);
+      setResults((prevResults) =>
+        prevResults.map((b) =>
+          b.id === book.id ? { ...b, shelf: shelf } : b
+        )
+      );
     }
-    };
-  
-    return (
-      <div className="search-page">
-        <div className="search-bar">
-          <button className="close-search" onClick={() => navigate(-1)}>
-            <FaArrowLeft />
-          </button>
-          <input
-            className="search-input"
-            type="text"
-            value={query}
-            onChange={handleSearchChange}
-            placeholder="Search books..."
-          
-          />
-    
-        </div>
-        <div className="search-results">
-          {error && <p>{error}</p>}
-          <ul className="searched-books">
-            {results.length > 0 ? (
-              results.map((book) => (
-                <li key={book.id} className="search-list">
-                  <Book book={book} onShelfChange={handleShelfChange} />
-                </li>
-              ))
-            ) : (
-              <h3>search for results.</h3>
-            )}
-          </ul>
-        </div>
-      </div>
-    );
   };
+
+  return (
+    <div className="search-page">
+      <div className="search-bar">
+        <button className="close-search" onClick={() => navigate(-1)}>
+          <FaArrowLeft />
+        </button>
+        <input
+          className="search-input"
+          type="text"
+          value={query}
+          onChange={handleSearchChange}
+          placeholder="Search books..."
+        />
+      </div>
+      <div className="search-results">
+        {error && <p>{error}</p>}
+        <ul className="searched-books">
+          {results.length > 0 ? (
+            results.map((book) => (
+              <li key={book.id} className="search-list">
+                <Book book={book} onShelfChange={handleShelfChange} />
+              </li>
+            ))
+          ) : (
+            <h3>search for results.</h3>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 SearchPage.propTypes = {
   booksOnShelves: PropTypes.array.isRequired,
